46e2422bae591b5ff3ebcab1094f6cecca1a3ef5
redrizoma <code@redrizoma.dev>
2026-04-07 12:17:11 -0600

    feat: add getHumanChoice function with prompt

--- script.js
+++ script.js
@@ -4,8 +4,13 @@ const options = {
   2: 'Scissors',
 };
 
+function getHumanChoice() {
+  return prompt('Choose your play: ');
+}
+
 function getComputerChoice() {
   return options[Math.floor(Math.random() * 3)];
 }
 
-console.log(getComputerChoice(3));
+console.log(getComputerChoice());
+console.log(getHumanChoice());
