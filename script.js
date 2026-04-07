ea7556317bf97989d0c6ce131a42aab4e711fd95
redrizoma <code@redrizoma.dev>
2026-04-07 11:54:54 -0600

    feat: add getComputerChoice function with random selection

---
+++ script.js
@@ -0,0 +1,11 @@
+const options = {
+  0: 'Rock',
+  1: 'Paper',
+  2: 'Scissors',
+};
+
+function getComputerChoice() {
+  return options[Math.floor(Math.random() * 3)];
+}
+
+console.log(getComputerChoice(3));
